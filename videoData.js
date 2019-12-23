32d1264c8414bf1e0e8f01ecf0dc6e64186f70f4
Julian Nicholls <juliannicholls29@gmail.com>
2019-12-23 21:03:54 +0000

    Re-factor parsing and interpretation

---
+++ videoData.js
@@ -0,0 +1,70 @@
+const humanSize = size => {
+  if (size > 1400000) return `${(size / 1048576).toFixed(1)}MB`;
+
+  return `${(size / 1024).toFixed(2)}KB`;
+};
+
+const parseData = videodata => {
+  const {
+    format: { format_long_name: format, duration, size, bit_rate },
+    streams,
+  } = videodata;
+
+  let video = null;
+
+  for (const stream of streams) {
+    if (stream.codec_type === 'video') {
+      if (video) {
+        console.warn('Multiple video streams');
+      }
+
+      video = stream;
+    }
+  }
+
+  if (!video) {
+    console.error('No video stream in file');
+
+    return {
+      format,
+      duration,
+      size,
+      bit_rate,
+      codec_name: '',
+      width: 0,
+      height: 0,
+    };
+  }
+
+  const { codec_name, width, height } = video;
+
+  return {
+    format,
+    duration,
+    size,
+    bit_rate,
+    codec_name,
+    width,
+    height,
+  };
+};
+
+const humanTime = length => {
+  length = Math.round(length);
+
+  const hours = length > 3600 ? `${Math.floor(length / 3600)}:` : '';
+  const mins = Math.floor((length % 3600) / 60);
+  const secs = length % 60;
+
+  if (hours === '' && mins === 0) return `${secs}s`;
+
+  return `${hours}${mins < 10 ? '0' + mins : mins}:${
+    secs < 10 ? '0' + secs : secs
+  }`;
+};
+
+module.exports = {
+  parseData,
+  humanSize,
+  humanTime,
+};
